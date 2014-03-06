573eb99cda215fc6192c6776557b83d819cf506a
lindehoff <jacob@cobmedia.se>
2014-03-06 10:47:09 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-simply.text({ title: 'Title', subtitle: 'Sub Tile', body: 'Body' });
+var init = function(){
+	simply.scrollable(true);
+	simply.style('small');
+	env = require('environment.js');
+
+	simply.on('singleClick', function(e){
+		if(e.button === "select"){
+			getRoom();
+		}
+	});
+	getRooms();
+}
+var getRooms = function(){
+  simply.text({ title: 'Title', subtitle: 'Sub Tile', body: 'Body' });
+}
+var getRoom = function(){
+	simply.text({
+		title: "Getting Room",
+		body: "Wait while we fetch devices in room"
+	}, true);
+init();
